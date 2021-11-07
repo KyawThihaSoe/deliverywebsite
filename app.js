2ec013f0dd214f0a0757f48cb5ac09e1f1062554
Kyaw Thiha Soe <sk6822386@gmail.com>
2021-11-07 02:50:16 -0800

    delivery

--- app.js
+++ app.js
@@ -5,3 +5,21 @@ menu.addEventListener('click',()=>{
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 });
+
+function scrollReveal(){
+    let revealpoint = 150;
+    let revealElement = document.querySelectorAll(".demo");
+
+    for(let i=0;i < revealElement.length;i++){
+        let windowHeight = window.innerHeight;
+        let revealtop = revealElement[i].getBoundingClientRect().top;
+
+        if(revealtop<windowHeight-revealpoint){
+            revealElement[i].classList.add("activeshow");
+        }else{
+            revealElement[i].classList.remove("activeshow");
+        }
+    }
+}
+window.addEventListener("scroll",scrollReveal);
+scrollReveal();
